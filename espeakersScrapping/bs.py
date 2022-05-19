ee160ea462d390e2ecf1ba3dc0cd5e2315d2cfe7
Oleksii Hnybida <alexgnibeda03@gmail.com>
2022-05-19 16:47:01 +0300

    adding new

--- espeakersScrapping/bs.py
+++ espeakersScrapping/bs.py
@@ -4,18 +4,15 @@ from selenium.webdriver.common.by import By
 
 
 urls = [url.strip() for url in open('result_email_urls.txt').readlines()]
-browser = webdriver.Chrome(executable_path='D:\portfolio_projects\espeakersScrapping\chromedriver_win32\chromedriver.exe')
-
-
+browser = webdriver.Chrome(executable_path='/Users/alexseyhnibida/Desktop/work/espeakersScrapping/chromedriver')
 
 with open('emails.txt', 'a') as f:
-    for url in urls:
-        browser.get(url)
+    for x in range(len(urls)):
+        browser.get(urls[x])
         browser.implicitly_wait(5)
         email = browser.find_element(by=By.XPATH, value="//span[@style='unicode-bidi: bidi-override; direction: rtl;']")
         print(email.text[::-1])
-        if urls.index(url) <= len(urls):
-            browser.execute_script("window.open('');")
-            browser.switch_to.window(browser.window_handles[urls.index(url)+1])
-
+        f.write(email.text[::-1]+'\n')
+        browser.get(urls[x+1])
     f.close()
+
